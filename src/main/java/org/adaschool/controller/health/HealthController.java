c0f5b7f5109488d02f26705445c4c1992ea01917
Santiago Naranjo <santiresox@hotmail.com>
2025-08-27 16:04:47 -0500

    Initial commit with a Get service

---
+++ src/main/java/org/adaschool/controller/health/HealthController.java
@@ -0,0 +1,13 @@
+package org.adaschool.controller.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String checkAPI(){
+        return "API working OK!";
+    }
+}
